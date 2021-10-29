3984da2ccd58c45acf72b2e80a995163695e8168
Kaedorm <kaedorm@gmail.com>
2021-10-29 18:19:47 +0200

    ajout localhost

--- app/middleware/cors.js
+++ app/middleware/cors.js
@@ -3,6 +3,7 @@ module.exports = function() {
      res.setHeader("Access-Control-Allow-Methods", "POST, GET");
      res.setHeader("Access-Control-Max-Age", "3600");
      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+     //res.setHeader("Access-Control-Allow-Headers: Accept");
       next();
     };
   }
