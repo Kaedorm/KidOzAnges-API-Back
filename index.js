834e1ae2edbb074b76f161bf570e482a6235441f
Kaedorm <kaedorm@gmail.com>
2021-11-01 21:24:28 +0100

    ajout alloweheaders

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: "http://localhost:3000",
-  credentials:true
+  credentials:true,
+  allowedHeaders: "authorization"
 }));
 app.use(session({
     secret: 'keyboard cat',
