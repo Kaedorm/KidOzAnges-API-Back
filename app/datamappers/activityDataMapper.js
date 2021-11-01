2c344af381e07888535c5aa3fc77e448c4cea6e6
Kaedorm <kaedorm@gmail.com>
2021-11-01 21:10:39 +0100

    retrait town du SQL activity

--- app/datamappers/activityDataMapper.js
+++ app/datamappers/activityDataMapper.js
@@ -5,8 +5,8 @@ const activityDataMapper = {
     submitActivity: async(title, description,slug,zipcode,town,free, user_Id) =>{
         try{
             const query = {
-                text: 'INSERT INTO "activity" (title, description,slug, zipcode, town, free, user_Id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, title, description, slug, zipcode, town, free, user_Id',
-                values: [title, description,slug, zipcode,town, free, user_Id]    
+                text: 'INSERT INTO "activity" (title, description,slug, zipcode, free, user_Id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, title, description, slug, zipcode, free, user_Id',
+                values: [title, description,slug, zipcode, free, user_Id]    
             };
             return await pool.query(query);
         
