0486794812835a2784fd3724fa145d4314129cf4
FabienNaveau <fabien.naveau@gmail.com>
2021-11-02 11:28:30 +0100

    Ajout de localhost dans les cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://kidozanges.netlify.app"],
   credentials:true,
   allowedHeaders: ["Authorization", "Content-Type"]
 }));
