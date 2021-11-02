e35cd64f0374cd0c7e36ed7830d25ac9cf716d22
Kaedorm <kaedorm@gmail.com>
2021-11-02 14:03:53 +0100

    essai JWT

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -7,9 +7,10 @@ const auth = {
     },
 
     authenticateToken: (req, res, next) => {
-        console.log(req.headers["Authorization"]);
-        const authHeader = req.headers["Authorization"];
-        const token = authHeader && authHeader.split(' ')[1];
+        console.log(req.headers["authorization"]);
+        const authHeader = req.headers["authorization"];
+        const token = authHeader.split(' ')[1];
+        console.log ("++++++++", token)
         // if there's no token we send an Unauthorized connection
         if(!token) {
             console.log("il n'y a pas de token")
