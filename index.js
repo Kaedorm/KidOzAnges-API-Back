919f0012e216e2f6b69927286f0ad9072e0b5035
Kaedorm <kaedorm@gmail.com>
2021-11-02 09:59:01 +0100

    repasse en localhost

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-  origin: "https://kidozanges.netlify.app",
+  origin: "http://localhost:3000",
   credentials:true,
   allowedHeaders: ["Authorization", "Content-Type"]
 }));
