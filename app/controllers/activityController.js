b23f3c2a49db6b54f9a318c35d4d75702dd4b76f
FabienNaveau <fabien.naveau@gmail.com>
2021-11-02 15:15:45 +0100

    Essais heroku jwt

--- app/controllers/activityController.js
+++ app/controllers/activityController.js
@@ -52,10 +52,10 @@ const activityController = {
             //send data in DB.
             const newActivity = await activityDataMapper.submitActivity(title, description, slug, zipcode, town, free, userId);
             // we take the id of the activity juste posted
-            const activityId = newActivity.rows[0].id
-            // we insert picture path in database with the id of the activity just posted
-            await activityDataMapper.insertPicture(req.file.path, activityId);
-            activityController.uploadPicture(req, res); //send the picture to AWS and delete it from public storage.
+            // const activityId = newActivity.rows[0].id
+            // // we insert picture path in database with the id of the activity just posted
+            // await activityDataMapper.insertPicture(req.file.path, activityId);
+            // activityController.uploadPicture(req, res); //send the picture to AWS and delete it from public storage.
             //send response to the front.
             res.status(200).json({message: "Nous vous remercions de votre proposition, celle-ci sera examinée avec le plus grand soin."})
 
