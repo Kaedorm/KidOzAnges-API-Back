cb2107c4a4987ce6e11c58f3d327fb097bac0673
Kaedorm <kaedorm@gmail.com>
2021-11-02 10:01:23 +0100

    retour netlify

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://kidozanges.netlify.app/",
   credentials:true,
   allowedHeaders: ["Authorization", "Content-Type"]
 }));
