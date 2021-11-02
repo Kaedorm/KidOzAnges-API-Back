97971fd49a5448a101319e38ef36264c8f7f3687
FabienNaveau <fabien.naveau@gmail.com>
2021-11-02 09:14:13 +0100

    Essai pour les cors

--- index.js
+++ index.js
@@ -13,20 +13,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials:true
-}));
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
-  }))
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use(cors({
+  origin: "https://kidozanges.netlify.app",
+  credentials:true,
+}));
 
 app.use(router);
 
