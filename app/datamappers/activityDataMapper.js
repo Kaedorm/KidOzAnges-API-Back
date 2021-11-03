b66f20ca13be5b30c47c2e52e11bb2c384d66b50
FabienNaveau <fabien.naveau@gmail.com>
2021-11-03 13:24:32 +0100

    Ajout d'un jeton en requte préparée

--- app/datamappers/activityDataMapper.js
+++ app/datamappers/activityDataMapper.js
@@ -17,7 +17,7 @@ const activityDataMapper = {
     submitActivity: async(title, description, town, slug,zipcode,free, user_Id) =>{
         try{
             const query = {
-                text: 'INSERT INTO "activity" (title, description,slug, town, zipcode, free, user_Id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, title, description, slug, zipcode, free, town, user_Id',
+                text: 'INSERT INTO "activity" (title, description,slug, town, zipcode, free, user_Id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, title, description, slug, zipcode, free, town, user_Id',
                 values: [title, description,slug, town, zipcode, free, user_Id]    
             };
             return await pool.query(query);
