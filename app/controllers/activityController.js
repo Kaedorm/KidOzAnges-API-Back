5da1dea2df0fc386271435468dc74afcda6e517a
Kaedorm <kaedorm@gmail.com>
2021-11-03 14:53:27 +0100

    UPLOAD PICTURE S3 LOCAL OK

--- app/controllers/activityController.js
+++ app/controllers/activityController.js
@@ -44,16 +44,16 @@ const activityController = {
             const newActivity = await activityDataMapper.submitActivity(title, description, slug, zipcode, town, free, userId);
             // we take the id of the activity juste posted
             const activityId = newActivity.rows[0].id
-            // we insert picture path in database with the id of the activity just posted
-            await activityDataMapper.insertPicture(req.file.path, activityId);
             
             //!activityController.uploadPicture(req, res); //send the picture to AWS and delete it from public storage.
-                const file = req.file
-                //console.log(file,"+++++++++++++++++++");
-                const result = await uploadFile(file)
-                console.log(result, "zzzzzzzzzzzzz");
-                await unlinkFile(file.path) //delete picture in app
-        
+            const file = req.file
+            //console.log(file,"+++++++++++++++++++");
+            const result = await uploadFile(file)
+            console.log(result, "zzzzzzzzzzzzz");
+            await unlinkFile(file.path) //delete picture in app
+            
+            // we insert picture path in database with the id of the activity just posted
+            await activityDataMapper.insertPicture(result.Location, activityId);
             //send response to the front.
             res.status(200).json({message: "Nous vous remercions de votre proposition, celle-ci sera examinée avec le plus grand soin."})
 
