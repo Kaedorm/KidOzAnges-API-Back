1a4aff8609bded6a0f61691149509ee45af6ca1a
Kaedorm <kaedorm@gmail.com>
2021-11-08 15:51:53 +0100

    comm de Find Best

--- app/datamappers/activityDataMapper.js
+++ app/datamappers/activityDataMapper.js
@@ -143,7 +143,7 @@ const activityDataMapper = {
         }
     },
 
-    findbestActivities: async ()=> {
+/*     findbestActivities: async ()=> {
         try {
             const query = {
                 text: `SELECT activity_has_rating.activity_id,ROUND(AVG(rate),1) AS "moyenne",activity.title, activity.slug, activity.town FROM activity_has_rating JOIN rating ON rating.id = activity_has_rating.note_id JOIN activity ON activity.id = activity_has_rating.activity_id GROUP BY activity_has_rating.activity_id
@@ -153,7 +153,7 @@ const activityDataMapper = {
         } catch (error) {
             console.error(error)
         }
-    }
+    } */
 };
 
 module.exports = activityDataMapper;
