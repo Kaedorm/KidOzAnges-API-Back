ea3b95e3a4e03af8f32c3f17d72f83dedce290bf
FabienNaveau <fabien.naveau@gmail.com>
2021-11-10 11:02:40 +0100

    Ajout du fait qu'un utilisateur ne puisse pas noter deux fois une activité

--- app/controllers/activityController.js
+++ app/controllers/activityController.js
@@ -116,6 +116,13 @@ const activityController = {
                 rate
             } = req.body;
             if (rate && rate > 0 && rate < 6) {
+                const userRatesActivity = await activityDataMapper.getUserWhoRatesActivity(userId, activityId);
+                const hasRates = userRatesActivity.rows.find(elm => elm.user_id == userId && elm.activity_id == activityId);
+                console.log(hasRates)
+                if(hasRates) {
+                    res.json({error: "Vous ne pouvez pas noter plusieurs fois cette activité"});
+                    throw new Error("Cette activité est déjà notée par cet utilisateur")
+                } 
                 const result = await activityDataMapper.rateActivity(rate)
 
                 const rateId = result.rows[0].id;
